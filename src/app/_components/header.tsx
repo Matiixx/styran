6bfc089b3205de09cb3fc5a74639cfbd690095df
Matiixx <87618425+Matiixx@users.noreply.github.com>
2024-11-10 14:08:45 +0100

    add simple header

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="absolute left-0 top-0 flex h-16 w-full items-center justify-between bg-gradient-to-b from-white/30 to-white/5 px-4 text-white">
+      <h1 className="text-2xl font-bold">Styran</h1>
+    </header>
+  );
+};
+
+export { Header };
--- src/app/api/auth/register/page.tsx
+++ src/app/api/auth/register/page.tsx
@@ -1,3 +1,5 @@
+import { Header } from "~/app/_components/header";
+
 import { RegisterForm } from "./registerForm";
 
 export const metadata = {
@@ -7,6 +9,7 @@ export const metadata = {
 const RegisterPage = () => {
   return (
     <main className="bg-gradient-background flex min-h-screen flex-col items-center justify-center">
+      <Header />
       <RegisterForm />
     </main>
   );
