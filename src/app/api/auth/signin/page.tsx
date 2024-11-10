e0dd5597358e16fbe2b9e258caca49030a58f130
Matiixx <87618425+Matiixx@users.noreply.github.com>
2024-11-10 15:45:22 +0100

    add login page

---
+++ src/app/api/auth/signin/page.tsx
@@ -0,0 +1,25 @@
+import { Header } from "~/app/_components/header";
+import { auth } from "~/server/auth";
+
+import { SignInForm } from "./signinForm";
+import { redirect } from "next/navigation";
+
+export const metadata = {
+  title: "Styran - Sign In",
+};
+
+export default async function SignInPage() {
+  const session = await auth();
+
+  if (session) {
+    redirect("/");
+    return null;
+  }
+
+  return (
+    <main className="bg-gradient-background flex min-h-screen flex-col items-center justify-center">
+      <Header />
+      <SignInForm />
+    </main>
+  );
+}
---
+++ src/app/api/auth/signin/signinForm.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+import Link from "next/link";
+
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { InputWithLabel } from "~/components/ui/input";
+
+const SignInSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(5),
+});
+
+const SignInForm = () => {
+  const {
+    formState: { errors },
+    register,
+    handleSubmit,
+  } = useForm<z.infer<typeof SignInSchema>>({
+    mode: "onTouched",
+    resolver: zodResolver(SignInSchema),
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    return signIn("credentials", {
+      email: data.email,
+      password: data.password,
+      redirect: false,
+    })
+      .then(console.log)
+      .catch(console.error);
+  });
+
+  return (
+    <Card className="w-full max-w-lg">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+      </CardHeader>
+
+      <CardContent>
+        <form className="flex w-full flex-col gap-4">
+          <InputWithLabel
+            label="Email"
+            type="email"
+            placeholder="Email"
+            error={!!errors.email}
+            errorMessage={errors.email?.message}
+            {...register("email")}
+          />
+
+          <InputWithLabel
+            label="Password"
+            type="password"
+            placeholder="Password"
+            error={!!errors.password}
+            errorMessage={errors.password?.message}
+            {...register("password")}
+          />
+        </form>
+      </CardContent>
+
+      <CardFooter className="flex justify-end gap-2">
+        <Link href="/api/auth/register">
+          <Button variant="outline">Create account</Button>
+        </Link>
+        <Button onClick={onSubmit}>Login</Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export { SignInForm };
