ffcdff4673c6f787ae9d50c1f72d6ce83fd28547
Matiixx <87618425+Matiixx@users.noreply.github.com>
2024-11-16 20:24:25 +0100

    add ticker field to task

---
+++ src/server/dbResolver.test.ts
@@ -0,0 +1,29 @@
+import { test } from "vitest";
+
+import map from "lodash/map";
+import padStart from "lodash/padStart";
+
+import { db } from "./db";
+
+test("update task tickers", async () => {
+  const tasks = await db.task.findMany({
+    include: { project: { select: { ticker: true } } },
+  });
+
+  await Promise.all(
+    map(tasks, (task, index) =>
+      db.task.update({
+        where: { id: task.id },
+        data: { ticker: generateTaskTicker(task.project.ticker, index) },
+      }),
+    ),
+  );
+});
+
+export const generateTaskTicker = (
+  projectTicker: string,
+  taskCount: number,
+) => {
+  const paddedTaskCount = padStart(`${taskCount + 1}`, 3, "0");
+  return `${projectTicker}-${paddedTaskCount}`;
+};
