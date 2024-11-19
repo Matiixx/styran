7a4d1a451764e71f5ec1a1db5a8fcb6da4cc0ce9
Matiixx <87618425+Matiixx@users.noreply.github.com>
2024-11-19 18:00:45 +0100

    fix border radius in small select

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -9,7 +9,7 @@ import { cn } from "~/lib/utils";
 
 const selectVariants = cva(
   "flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
-  { variants: { size: { sm: "h-6 rounded-xs" } } },
+  { variants: { size: { sm: "h-6 rounded-sm" } } },
 );
 
 const Select = SelectPrimitive.Root;
