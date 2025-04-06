d36c01274988aabd1cf838136beca51fad63bc2f
Matiixx <87618425+Matiixx@users.noreply.github.com>
2025-04-06 12:09:15 +0200

    set locale to have monday first day of wekk

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -2,10 +2,12 @@ import dayjs, { type Dayjs } from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
+import "dayjs/locale/en-gb";
 
 dayjs.extend(relativeTime);
 dayjs.extend(timezone);
 dayjs.extend(utc);
+dayjs.locale("en-gb");
 
 export default dayjs;
 export type { Dayjs };
