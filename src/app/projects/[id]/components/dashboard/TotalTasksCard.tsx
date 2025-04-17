2e22190ea69c1e79ba517a7c64ab7012056853c3
Matiixx <87618425+Matiixx@users.noreply.github.com>
2025-04-17 17:39:01 +0200

    enhance TotalTasksCard to display percentage change in task counts with conditional styling

--- src/app/projects/[id]/components/dashboard/TotalTasksCard.tsx
+++ src/app/projects/[id]/components/dashboard/TotalTasksCard.tsx
@@ -2,6 +2,7 @@ import { ArrowUp, List } from "lucide-react";
 
 import { Card, CardContent } from "~/components/ui/card";
 import { Skeleton } from "~/components/ui/skeleton";
+import { cn } from "~/lib/utils";
 
 const TotalTasksCard = ({
   currentMonthTasksCount,
@@ -10,6 +11,12 @@ const TotalTasksCard = ({
   currentMonthTasksCount: number | null;
   previousMonthTasksCount: number | null;
 }) => {
+  const change =
+    currentMonthTasksCount !== null && previousMonthTasksCount !== null
+      ? ((currentMonthTasksCount - previousMonthTasksCount) /
+          previousMonthTasksCount) *
+        100
+      : null;
   return (
     <Card disableHover className="w-full">
       <CardContent>
@@ -34,14 +41,21 @@ const TotalTasksCard = ({
           {currentMonthTasksCount !== null &&
           previousMonthTasksCount !== null ? (
             <>
-              <ArrowUp className="mr-1 h-4 w-4 text-green-500" />
-              <span className="font-medium text-green-500">
-                {previousMonthTasksCount === 0
-                  ? `+inf`
-                  : ((currentMonthTasksCount - previousMonthTasksCount) /
-                      previousMonthTasksCount) *
-                    100}
-                %
+              <ArrowUp
+                className={cn(
+                  "mr-1 h-4 w-4",
+                  change && change > 0
+                    ? "text-green-500"
+                    : "-rotate-180 text-red-500",
+                )}
+              />
+              <span
+                className={cn(
+                  "font-medium",
+                  change && change > 0 ? "text-green-500" : "text-red-500",
+                )}
+              >
+                {previousMonthTasksCount === 0 ? `+inf` : change}%
               </span>
               <span className="ml-1 text-muted-foreground">
                 from last month
