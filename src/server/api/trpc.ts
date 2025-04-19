ac1320541c0a53545a9ee2ed9114b8a5bc1db0ee
Matiixx <87618425+Matiixx@users.noreply.github.com>
2025-04-19 11:56:21 +0200

    add response status to logging middleware

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -101,7 +101,10 @@ const timingMiddleware = t.middleware(async ({ next, path }) => {
   const result = await next();
 
   const end = Date.now();
-  console.log(`[TRPC] ${path} took ${end - start}ms to execute`);
+  const statusCode = result.ok ? 200 : 500;
+  console.log(
+    `[TRPC] ${path} took ${end - start}ms to execute - Status: ${statusCode}`,
+  );
 
   return result;
 });
